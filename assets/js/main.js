d77cfc51c63c718e997f2e3dc9f01a8fe52795a2
paranerd <paranerd.development@gmail.com>
2024-04-08 16:20:24 +0200

    Integrated copy-code into main

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,7 @@ document.addEventListener(
     setupArrowDown();
     setupCategories();
     setupCollapsibles();
+    setupCopyButtons();
   },
   false
 );
@@ -116,4 +117,45 @@ function isInViewport(elem) {
     rect.left < (window.innerWidth || html.clientWidth) &&
     rect.right > 0
   );
+}
+
+function setupCopyButtons() {
+  document.querySelectorAll('pre > code').forEach((codeblock) => {
+    const container = codeblock.parentNode.parentNode;
+
+    const copybutton = document.createElement('button');
+    copybutton.classList.add('copy-code', 'light');
+    copybutton.innerText = 'Copy';
+
+    function copyingDone() {
+      copybutton.innerText = 'Copied!';
+      setTimeout(() => {
+        copybutton.innerText = 'Copy';
+      }, 2000);
+    }
+
+    copybutton.addEventListener('click', () => {
+      if ('clipboard' in navigator) {
+        navigator.clipboard.writeText(codeblock.textContent);
+        copyingDone();
+      }
+    });
+
+    if (container.classList.contains('highlight')) {
+      container.appendChild(copybutton);
+    } else if (container.parentNode.firstChild == container) {
+      // td containing LineNos
+    } else if (
+      codeblock.parentNode.parentNode.parentNode.parentNode.parentNode
+        .nodeName == 'TABLE'
+    ) {
+      // table containing LineNos and code
+      codeblock.parentNode.parentNode.parentNode.parentNode.parentNode.appendChild(
+        copybutton
+      );
+    } else {
+      // code blocks not having highlight as parent class
+      codeblock.parentNode.appendChild(copybutton);
+    }
+  });
 }
